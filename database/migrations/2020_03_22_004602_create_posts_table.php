9c5481137349a486f39aa5e1e2ab9f930e900dff
gibmyx <gibmyxjobs@gmail.com>
2020-03-21 21:27:02 -0400

    configurando pusher y los canales

--- database/migrations/2020_03_22_004602_create_posts_table.php
+++ database/migrations/2020_03_22_004602_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
             $table->text('body');
--- database/migrations/2020_03_22_004619_create_comments_table.php
+++ database/migrations/2020_03_22_004619_create_comments_table.php
@@ -15,9 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('post_id')->unsigned();
+            $table->unsignedBigInteger('post_id')->unsigned();
             $table->foreign('post_id')->references('id')->on('posts');
-            $table->integer('user_id')->unsigned();
+            $table->unsignedBigInteger('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->text('coment');
             $table->timestamps();
